942f050dd85a4e69dd2bebe42c74bf803e60d869
Orine <orineb01@gmail.com>
2020-10-21 13:14:54 +0300

    with jest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+export default {
+    
+    testEnvironment: "node",
+    moduleDirectories:[ "src", "transparent"],
+    moduleFileExtensions: [
+      "js",
+      "json",
+      "jsx",
+      "ts",
+      "tsx",
+      "node"
+    ],
+    clearMocks: true,
+    
+    moduleDirectories: ["node_modules"]
+
+  }
+  
